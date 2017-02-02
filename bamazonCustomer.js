bb58551237e8c2de8931f660197c4fe439eb2550
Rob Lovelett <rlovelett@gmail.com>
2017-02-02 17:58:30 -0600

    starting questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,4 @@
 /*
-5. Then create a Node application called `bamazonCustomer.js`. Running this
-application will first display all of the items available for sale. Include the
-ids, names, and prices of products for sale.
-
 6. The app should then prompt users with two messages. * The first should ask
 them the ID of the product they would like to buy. * The second message should
 ask how many units of the product they would like to buy.
@@ -20,6 +16,7 @@ cost of their purchase.
 
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require("cli-table");
 var config = require("./configs.js")
 
 var connection = mysql.createConnection({
@@ -30,11 +27,33 @@ var connection = mysql.createConnection({
     database: config.database
 });
 
-connection.connect();
-
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    listProducts();
 });
 
-console.log(connection);
+/*
+5. Then create a Node application called `bamazonCustomer.js`. Running this
+application will first display all of the items available for sale. Include the
+ids, names, and prices of products for sale.
+*/
+var listProducts = function() {
+    var query = "SELECT * FROM products;";
+    connection.query(query, function (error, results, fields) {
+        if (error) throw error;
+        var table = new Table({ //instantiate
+            head: ['Item ID', 'Product Name', 'Department Name', 'Price', 'Stock Quantity']
+        });
+        var products = JSON.parse(JSON.stringify(results));
+        for (i=0; i<products.length; i++) {
+            table.push(
+                [products[i].item_id, products[i].product_name, products[i].department_name,
+                products[i].price, products[i].stock_quantity]
+            );
+        };
+        console.log(table.toString());
+    });
+};
+ 
+
