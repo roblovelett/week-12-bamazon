b384b8a15433fe2a4b781f7c0cd3c1a120a968d4
Rob Lovelett <rlovelett@gmail.com>
2017-02-02 22:55:05 -0600

    id matching working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,4 @@
 /*
-6. The app should then prompt users with two messages. * The first should ask
-them the ID of the product they would like to buy. * The second message should
-ask how many units of the product they would like to buy.
 
 7. Once the customer has placed the order, your application should check if
 your store has enough of the product to meet the customer's request. * If not,
@@ -29,7 +26,6 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
     listProducts();
 });
 
@@ -53,7 +49,71 @@ var listProducts = function() {
             );
         };
         console.log(table.toString());
+        askProducts(products);
     });
 };
  
+function askProducts(products) {
+    /*6. The app should then prompt users with two messages.*/
+    var product_id; //init
+    var questions = [
+  {
+    type: 'input',
+    name: 'id',
+    /* The first should ask them the ID of the product they would like to buy. */
+    message: 'Enter ID of product you are buying: ',
+    validate: function (val) {
+        product_id = Number(val); //make ID a num
+        for (i=0; i < products.length; i++) {
+            if (product_id === Number(products[i].item_id)) { 
+                return true; //if match
+            };
+        };
+        return 'ID ' + product_id + ' does not exist. Please enter a valid ID.'; //if no match
+    }
+  },
+  {
+    type: 'input',
+    name: 'amount',
+    /* The second message should ask how many units of the product they would like to buy.*/
+    message: 'How many units are you purchasing? ',
+    validate: function (val) {
+        var product_qty = Number(product.stock_quantity);
+        if (val.match([1-9999] && val < product.stock_quantity)) {
+            var stock_qty = product.stock_quanity - val; //subtract val from stock in product table
+            if (stock_qty < 0) {
+                return 'Not enough in stock to purchase this amount.';
+            } else {
+                var total = product.price * val;
+                var query = "UPDATE products";
+                query += "SET ";
+                query += "stockquantity = " + stock_qty;
+                query += "WHERE item_id = " + product.item_id;
+                connection.query(query, function (error, results, fields) {
+                    if (error) throw error;
+                });
+                return 'Purchased. Total: ' + total;
+            };
+        };
+        return 'Not a valid amount. Please enter positive numerical values.';
+    }
+  },
+  {
+    type: 'input',
+    name: 'continue',
+    message: 'Do you want to make another purchase (Y/N) ? ',
+    validate: function (val) {
+      var pass = value.match(/y/gi);
+      if (pass) {
+        listProducts();
+      };
+      return 'Goodbye!';
+    }
+  }
+];
 
+inquirer.prompt(questions).then(function (answers) {
+  console.log(JSON.stringify(answers, null, '  '));
+});
+
+};
